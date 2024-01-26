db1c130b79e646b32139ffc11a92b4983fa91fe9
Dillona25 <Dillyb00by49@gmail.com>
2024-01-25 20:50:37 -0600

    Added cors

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const routes = require("./routes");
 const { PORT = 3001 } = process.env;
 const app = express();
 
+const cors = require("cors");
+
+app.use(cors());
+
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (r) => {
   console.log("connected to DB", r);
 });
